9b9ec6ca3f2ef1ff01402492c2d95a6dff29fb86
cahin7692 <cahin7692@7KWL6Y1.Student.UGDSB.ED>
2018-02-28 11:15:36 -0500

    This is my second commit.

---
+++ NetBeansProjects/AreaOfARectangle/src/areaofarectangle/AreaOfARectangle.java
@@ -0,0 +1,29 @@
+/*
+ * Caleb Hinderle
+ * AreaOfARectangle.java
+ * Find the area of a 5.7 by 4.8 rectangle
+ * 27/02/2018
+ */
+
+package areaofarectangle;
+
+/**
+ *
+ * @author cahin7692
+ */
+public class AreaOfARectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double length, width, area;
+        length = 5.7;
+        width = 4.8;
+        area = 0;
+        //calculations
+        area= length*width;
+        System.out.println("the are of a circle with the length of " +length +" and the width of " + width +" is " +area);
+}
+    
+}
